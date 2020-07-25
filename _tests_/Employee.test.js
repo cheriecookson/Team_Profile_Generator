8e1a870340fa1e916633db1dec8749faf594d31a
Cherie Cookson <cheriecookson@gmail.com>
2020-07-25 16:25:16 -0500

    employee test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates a employee object', () => {
+    const employee = new Employee('bob', 9, `bob@email`);
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+  });
+
+// test(`gets employee name`, () => {
+//     const employee = new Employee(`bob`);
+//     expect(employee.getName()).toEqual(expect.any(String));
+// });
+
+// test(`gets employee id`, () => {
+//     const employee = new Employee(9);
+//     expect(employee.getId).toEqual(expect.any(Number));
+// });
+
+// test(`gets employee email`, () => {
+//     const employee = new Employee(`bob@email`);
+//     expect(employee.getEmail).toEqual(expect.any(String));
+// });
+
